a64197578d23cac17d1b71e0f51c494c57271f3c
dru18 <dru41821@gmail.com>
2019-09-09 19:12:05 +0530

    Signed-off-by: dru18 <dru41821@gmail.com>
    	modified:   install.sh
    	modified:   uninstall.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+# Uninstall cls.
+sudo bash uninstall.sh
+
+echo -e "\n[*] Installing dependencies...\n"
 # Install dependencies.
 sudo apt install cowsay toilet
 
+echo -e "\n[*] Installing cls...\n"
 # Make 'cls.sh' executable.
 sudo chmod 755 cls.sh
 
@@ -11,3 +16,4 @@ sudo cp cls.sh /usr/bin/
 
 # Make soft link for /usr/bin/cls.sh to /bin/cls
 sudo ln -s /usr/bin/cls.sh /bin/cls
+echo "[+] cls installed successfully." | cowsay -f tux | toilet -f term --gay
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
+echo "[*] Uninstalling cls..."
 # Remove soft link 'cls' from /bin/ directory.
 sudo rm /bin/cls
 
 # Remove 'cls.sh' from /usr/bin/ directory.
 sudo rm /usr/bin/cls.sh
 
+echo -e "\n[*] Uninstalling dependencies..."
 # purge dependencies.
 sudo apt purge cowsay toilet
 
 # autoremove remaining files of dependencies.
 sudo apt autoremove
+echo -e "\n[+] cls uninstalled."
